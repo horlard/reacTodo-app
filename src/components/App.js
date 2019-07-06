80d8cd97545b8e35231b7a3a59424c1d6798bc17
horlard <oladipuposaulih@gmail.com>
2019-07-06 10:01:33 +0100

    imported react-router

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,31 @@
 import React from 'react'
+import {BrowserRouter as Router,Route} from 'react-router-dom';
+import Todocreate from '../todo-feature/todoCreate';
+import Todoedit from '../todo-feature/todoEdit';
+import Todolist from '../todo-feature/todoList';
+import Tododelete from '../todo-feature/todoDelete';
+import Header from './header';
 
-const App = () => {
-    return (
-        <div>
-            App
-        </div>
-    )
+
+
+class App extends React.Component{
+    render() {
+        return (
+            <div>
+                <Router>
+                <div className='ui container'>
+                    <Header/>
+                    <Route path='/' exact component={Todolist}/>
+                    <Route path='/new' exact component={Todocreate}/>
+                    <Route path='/edit' exact component={Todoedit}/>
+                    <Route path='/delete' exact component={Tododelete} />
+                </div>
+            </Router>
+            </div>
+            
+        )
+    }
+    
 }
 
 export default App;
