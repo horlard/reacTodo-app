a84019c19207f16e57289a54537521565fb15ec8
horlard <oladipuposaulih@gmail.com>
2019-07-06 19:03:56 +0100

    added the create todo button

--- src/todo-feature/todoList.js
+++ src/todo-feature/todoList.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import {Link} from 'react-router-dom';
 
 export default function todoList() {
     return (
         <div>
-            todoList
+            <div style={{textAlign : 'right',marginTop:'20px'}}>
+                    <Link to='/new' className='ui button primary'>
+                        Create Todo
+                    </Link>
+                </div>
         </div>
     )
 }
