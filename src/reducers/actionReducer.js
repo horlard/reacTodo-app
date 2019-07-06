75c623c4c3c9425abd0c369fb24431957ccde2c7
horlard <oladipuposaulih@gmail.com>
2019-07-06 19:01:56 +0100

    reducer for the CRUD operation

---
+++ src/reducers/actionReducer.js
@@ -0,0 +1,18 @@
+import _ from 'lodash';
+
+export default (state={},action) {
+    switch(action.type) {
+        case 'TODO_CREATE' : 
+            return {...state,[action.payload.id] : [action.payload]}
+        case 'FETCH_TODO' : 
+            return {...state,[action.payload.id] : [action.payload]}
+        case 'EDIT_TODO' :
+            return {...state,[action.payload.id] : [action.payload]}
+        case 'DELETE_TODO' : 
+            return _.omit(state,action.payload)
+        case 'FETCH_TODOS' : 
+            return {...state,..._.mapKeys(action.payload,'id')}
+        default :
+            return state;
+    }
+}
