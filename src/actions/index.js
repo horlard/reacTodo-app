b53ce1129118be1a9a7cf30602ea19c6f1693b9f
horlard <oladipuposaulih@gmail.com>
2019-07-06 18:57:44 +0100

    added a bunch of action creators for the CRUD operations

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,6 @@
+import Todoapi from '../api/todosapi';
+import History from '../components/history';
+
 export const Signin=(clientId)=> {
     return {
         type : 'SIGN_IN',
@@ -8,4 +11,46 @@ export const Signout =()=> {
     return{
         type: 'SIGN_OUT'
     }
+}
+export const todocreate = formValues => async dispatch => {
+    const response = await Todoapi.post('/todos',formValues);
+
+    dispatch({
+        type:'TODO_CREATE',
+        payload : response.data
+    })
+    History.push('/');
+}
+
+export const fetchtodo = id => async dispatch=> {
+    const response = Todoapi.get(`/todos/${id}`);
+
+    dispatch ({
+        type: 'FETCH_TODO',
+        payload : response.data
+    })
+}
+
+export const fetchtodos = async dispatch => {
+    const response =  await Todoapi.get('/todos');
+    dispatch({
+        type: 'FETCH_TODOS',
+        payload : response.data
+    })
+}
+
+export const editodo = (formValues,id) => async dispatch => {
+    const response=await Todoapi.patch(`/todos/${id}`,formValues)
+    dispatch({
+        type : 'EDIT_TODO',
+        payload : response.data
+    })
+}
+
+export const deletetodo = id => async dispatch => {
+     await Todoapi.delete(`/todos/${id}`)
+    dispatch({
+        type : 'DELETE_TODO',
+        payload: id
+    })
 }
