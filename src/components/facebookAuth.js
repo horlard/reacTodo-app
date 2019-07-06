d66b198deb96322a3c333ae9d55247a159016e4a
horlard <oladipuposaulih@gmail.com>
2019-07-06 11:12:21 +0100

    imported connect and created an access to the redux-store

---
+++ src/components/facebookAuth.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react'
+import FacebookLogin from 'react-facebook-login';
+import {connect} from 'react-redux';
+import {Signin} from '../actions'
+
+class FacebookAuth extends Component {
+    renderButton(){
+        if(this.props.isSignedIn) {
+            return <button className='ui red google button'>Sign out</button>
+        }else {
+           return  <FacebookLogin
+            appId="325255145050351"
+            autoLoad={false}
+            fields="name,email,picture"
+            onClick={this.componentClicked.bind(this)}
+            callback={this.responseFacebook} />
+        }
+    }
+    componentClicked(){
+        console.log(this.props);
+        
+    }
+    responseFacebook=(response)=>{
+        this.props.Signin(response.userID)
+        console.log(response)
+        
+    }
+    render() {
+        console.log(this.props)
+        return (
+            <div>
+                {
+                    this.renderButton()
+                }
+            </div>
+        )
+    }
+}
+
+const mapStateToProps=(state)=>{
+    return {isSignedIn:state.facebookAuth.isSignedIn}
+}
+export default connect(mapStateToProps,{Signin})(FacebookAuth);
