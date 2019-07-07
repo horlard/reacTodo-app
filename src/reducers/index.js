c89c437fec0885112f1a9aaa6dca5bddef37f407
horlard <oladipuposaulih@gmail.com>
2019-07-07 22:41:33 +0100

    added the actionReducer reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,12 @@
 import {combineReducers} from 'redux';
 import FacebookAuth from './facebookauthReducer';
 import {reducer} from 'redux-form';
+import ActionReducer from './actionReducer';
 
 export default combineReducers({
     facebookAuth : FacebookAuth,
-    form:reducer
+    form:reducer,
+    actionReducer : ActionReducer
+
+
 })
