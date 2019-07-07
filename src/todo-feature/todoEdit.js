a316d2d3cdfce67ae0f148c1befe28aed0c59d4d
horlard <oladipuposaulih@gmail.com>
2019-07-07 22:43:29 +0100

    gave access to redux store

--- src/todo-feature/todoEdit.js
+++ src/todo-feature/todoEdit.js
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from 'react';
+import _ from 'lodash';
+import {connect} from 'react-redux';
+import {fetchtodo,editodo} from '../actions';
+import Todoform from '../components/todoForm';
 
-export default function todoEdit() {
-    return (
-        <div>
-            todoEdit
-        </div>
-    )
+class todoEdit extends React.Component {
+    componentDidMount() {
+        this.props.fetchtodo(this.props.match.params.id)
+        console.log(this.props)
+    }
+    onSubmit=(formValues)=> {
+        this.props.editodo(formValues,this.props.match.params.id)
+    }
+    render() {
+        return (
+            <div>
+                <Todoform onSubmit={this.onSubmit} title='Edit' initialValues={_.pick(this.props.todo,'todo')}/>
+            </div>
+        )
+    }
+    
 }
+const mapStateToProps = (state,ownProps)=> {
+    return {todo : state.actionReducer[ownProps.match.params.id]}
+}
+export default connect(mapStateToProps,{fetchtodo,editodo})(todoEdit);
