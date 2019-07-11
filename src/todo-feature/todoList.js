617487f2d4722f44c645b3f7ee7b78d9e6940969
horlard <oladipuposaulih@gmail.com>
2019-07-11 16:36:33 +0100

    created conditions

--- src/todo-feature/todoList.js
+++ src/todo-feature/todoList.js
@@ -2,40 +2,57 @@ import React from 'react'
 import {Link} from 'react-router-dom';
 import {connect} from 'react-redux';
 import {fetchtodos} from '../actions';
+import Loader from '../components/loader';
+import '../../src/style.css'
 
 class todoList extends React.Component {
     componentDidMount(){
         this.props.fetchtodos();
-        console.log(this.props.todos)
     }
     renderList(){
-        return this.props.todos.map(todo=>{
-            return (
-                <div className='item' key={todo.id}>
-                    {this.renderActions(todo)}
-                    <i class="hand point right outline icon"></i>
-                    <div className='content'>
-                        <div className='header'>
-                            {todo.todo}
+        if(this.props.isSignedIn) {
+            return this.props.todos.map(todo=>{
+                return (
+                    <div className='item' key={todo.id}>
+                        {this.renderActions(todo)}
+                        <i className="hand point right outline icon"></i>
+                        <div className='content'>
+                            <div className='header' key={todo.id}>
+                                {todo.todo}
+                            </div>
                         </div>
+                        
                     </div>
-                    
-                </div>
-            )
-        })
+                )
+            })
+        }
+
+        return <Loader />
+        
        
     }
     renderActions = (todo)=> {
+        if(todo.clientId=== this.props.clientID) {
+            return (
+                <div className='right floated content'>
+                        <Link to={`/edit/${todo.id}`} className='ui button primary'>
+                            Edit
+                        </Link>
+                        <Link to={`/delete/${todo.id}`} className='ui negative button' style={{backgroundColor:'yellowgreen'}}>
+                            Done
+                        </Link>
+                    </div>
+            )
+        }
         return (
-            <div className='right floated content'>
-                    <Link to={`/edit/${todo.id}`} className='ui button primary'>
-                        Edit
-                    </Link>
-                    <Link to={`/delete/${todo.id}`} className='ui negative button'>
-                        Delete
-                    </Link>
-                </div>
+            <div className='right floated content'  id='floated-content'>
+                <img src={this.props.picture}  className='profile_pics'/>
+               {this.props.name}         
+            </div>
         )
+        
+
+        
     }
     renderCreate=()=> {
         if(this.props.isSignedIn) {
@@ -48,6 +65,7 @@ class todoList extends React.Component {
                     </div>
             )
         }
+        return ''
             
         
     }
@@ -68,7 +86,10 @@ class todoList extends React.Component {
 const mapStateToProps = (state)=> {
     return {
         todos:Object.values(state.actionReducer),
-        isSignedIn : state.facebookAuth.isSignedIn
+        isSignedIn : state.facebookAuth.isSignedIn,
+        clientID:state.facebookAuth.clientId,
+        name : state.facebookAuth.name,
+        picture : state.facebookAuth.picture
     }
 }
 export default connect(mapStateToProps,{fetchtodos})(todoList)
