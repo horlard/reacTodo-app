51c68663d4bea02b3ebb4f38764f7e0747004590
horlard <oladipuposaulih@gmail.com>
2019-07-11 16:34:05 +0100

    passed in picture and username

--- src/reducers/facebookauthReducer.js
+++ src/reducers/facebookauthReducer.js
@@ -1,12 +1,13 @@
 const initial = {
     isSignedIn : null,
-    clientId : null
+    clientId : null,
+    name : null
 }
 
 export default (state=initial,action) => {
     switch(action.type) {
         case 'SIGN_IN' :
-            return {...state,isSignedIn:true,clientId: action.payload}
+            return {...state,isSignedIn:true,clientId: action.payload.clientId,name: action.payload.name,picture:action.payload.picture}
         case 'SIGN_OUT' :
             return {...state,isSignedIn:false}
         default : 
