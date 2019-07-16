67647b676cfdf0a9f3b026eacc0711d4d66b8d1c
horlard <oladipuposaulih@gmail.com>
2019-07-17 00:06:29 +0100

    update

--- src/api/todosapi.js
+++ src/api/todosapi.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 let baseURL = 'http://localhost:3002';
 const hostname = window && window.location && window.location.hostname;
 console.log(hostname);
-if(hostname === 'https://react-todo-mgt.netlify.com/') {
+if(hostname === 'react-todo-mgt.netlify.com') {
   baseURL = 'https://react-todo-db.herokuapp.com';
 }
 
