5003860f4aa1d9ccc83222444281b5cbacad8482
horlard <oladipuposaulih@gmail.com>
2019-07-16 23:57:43 +0100

    update

--- src/api/todosapi.js
+++ src/api/todosapi.js
@@ -1,5 +1,12 @@
 import axios from 'axios'
+let baseURL = 'http://localhost:3002';
+const hostname = window && window.location && window.location.hostname;
+
+if(hostname === 'https://react-todo-mgt.netlify.com') {
+  baseURL = 'https://react-todo-db.herokuapp.com';
+}
+
 
 export default axios.create({
-    baseURL: 'http://localhost:3002'
+    baseURL
 })
