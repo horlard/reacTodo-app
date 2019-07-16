6fba04cc4e20534b3ed5de30e1214c00a5299735
horlard <39794876+horlard@users.noreply.github.com>
2019-07-17 00:27:28 +0100

    Update

--- src/api/todosapi.js
+++ src/api/todosapi.js
@@ -3,7 +3,7 @@ let baseURL = 'http://localhost:3002';
 const hostname = window && window.location && window.location.hostname;
 console.log(hostname);
 if(hostname === 'react-todo-mgt.netlify.com') {
-  baseURL = 'https://react-todo-db.herokuapp.com';
+  baseURL = 'https://cors-anywhere.herokuapp.com/https://react-todo-db.herokuapp.com';
 }
 
 
