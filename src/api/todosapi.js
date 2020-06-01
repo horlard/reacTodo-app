a39966a690b9b2b7cf1abffa9b7736fe27683360
horlard <oladipuposaulih@gmail.com>
2020-06-01 23:54:01 +0100

    update

--- src/api/todosapi.js
+++ src/api/todosapi.js
@@ -1,10 +1,7 @@
 import axios from 'axios'
-let baseURL = 'http://localhost:3000';
 const hostname = window && window.location && window.location.hostname;
 console.log(hostname);
-if(hostname === 'react-todo-mgt.netlify.app') {
-  baseURL = 'https://cors-anywhere.herokuapp.com/https://react-todo-db.herokuapp.com';
-}
+  let baseURL = 'https://cors-anywhere.herokuapp.com/https://react-todo-db.herokuapp.com';
 
 
 export default axios.create({
